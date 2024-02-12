15504a1f1c58ce2fa4066bf9c993fc15c0d1518f
Brice Cabioch <cabioch.brice@gmail.com>
2024-02-12 21:13:27 +0100

    completed iteration 4

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -6,10 +6,23 @@ import ProductTable from "./ProductTable";
 function ProductsPage() {
   const [products, setProducts] = useState(jsonData);
 
+  // Filter products
+  const filterProducts = (str) => {
+    let productsCopy;
+    if (str === "") {
+      productsCopy = [...jsonData];
+    } else {
+      productsCopy = jsonData.filter((product) => {
+        return product.name.includes(str);
+      });
+    }
+    setProducts(productsCopy);
+  };
+
   return (
     <div>
       <h1>IronStore</h1>
-      <SearchBar />
+      <SearchBar filteredProducts={filterProducts} />
       <ProductTable products={products} />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,18 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar(props) {
+  const [searchItem, setSearchItem] = useState("");
+
+  const handleSearch = (e) => {
+    const searchedItem = e.target.value;
+    props.filteredProducts(searchedItem);
+    setSearchItem(searchedItem);
+  };
+
   return (
     <div>
       <p>Search</p>
-      <input name="searchInput" />
+      <input name="searchInput" value={searchItem} onChange={handleSearch} />
       <br />
       <label>
         <input type="checkbox" name="myCheckbox" /> Only show products in stock
